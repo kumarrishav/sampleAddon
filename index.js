58ddef72cbe6a2a43be43a2b6d203bde6442d320
Kumar Rishav <rishav006@gmail.com>
2026-02-13 03:04:58 -0800

    Clean up comments in index.js
    Removed unnecessary comments about DOM readiness.

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ if (document.documentElement) {
   initialize();
 }
 
-// Otherwise, we need to wait for the DOM to be ready before
-// starting initialization since add-ons are injected
-// *before* `document.documentElement` is defined.
+
 else {
   window.addEventListener('DOMContentLoaded', initialize);
 }
